19d62501a1d316cb3deac556b842de58b6e67956
Amandaleeanne Schock <145935534+Amandaleeanne@users.noreply.github.com>
2025-12-04 11:01:37 -0800

    Restructured files for ease of deployment

---
+++ server/models/meal.js
@@ -0,0 +1,49 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/index.js';
+
+const Meal = sequelize.define('Meal', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  foodId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  foodName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  servingSize: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  servingUnit: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'g',
+  },
+  servings: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 1,
+  },
+  nutrition: {
+    type: DataTypes.JSONB,
+    allowNull: true,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  isPlanned: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+}, {
+  tableName: 'meals',
+});
+
+export default Meal;
